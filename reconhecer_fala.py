659f0a29b9dfaad84745cd8df46109fc6cf4b9cc
SamanthaBulhosa <78524117+SamanthaBulhosa@users.noreply.github.com>
2025-03-16 23:24:11 +0000

    Limite de tempo para reconhecer a fala
    timeout(tempo máximo de espera para começar a escutar) e phrase_time_limit(tempo máximo de captura de fala). Se o programa parar de gravar rápido demais, aumente esses valores

--- reconhecer_fala.py
+++ reconhecer_fala.py
@@ -5,6 +5,6 @@ rec = sr.Recognizer()
 with sr.Microphone() as mic:
     rec.adjust_for_ambient_noise(mic)
     print("Fale algo que vou gravar em texto: ")
-    audio = rec.listen(mic)
+    audio = rec.listen(mic, timeout=5, phrase_time_limit=10)
     texto = rec.recognize_google(audio, language="pt-BR")
     print(texto)
